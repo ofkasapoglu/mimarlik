8881ce07e8b26698bea6d5b18f17d7cc59245bcb
omer <kasapoglu2100@gmail.com>
2025-08-16 23:03:08 +0300

    Add backend URL env variable7

--- sila-frontend/src/App.js
+++ sila-frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 // Pages
 import Home from './pages/Home';
+// import Login from './pages/Login';
+// import Register from './pages/Register';
 import BlogDetail from './pages/BlogDetail';
 import CreateBlog from './pages/CreateBlog';
 import AdminLogin from './pages/admin/AdminLogin';
@@ -22,6 +24,7 @@ import Contact from './pages/Contact';
 import AboutManagement from './pages/admin/AboutManagement';
 
 // Contexts
+import { AuthProvider } from './contexts/AuthContext';
 import { AdminProvider } from './contexts/AdminContext';
 
 // Components
@@ -31,35 +34,41 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <AdminProvider>
-      <div className="min-h-screen bg-gray-100">
-        <Navbar />
-        <div className="container mx-auto px-4 py-8">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/blogs" element={<Blogs />} />
-            <Route path="/blog/:id" element={<BlogDetail />} />
-            <Route path="/create-blog" element={<PrivateRoute><CreateBlog /></PrivateRoute>} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/portfolio/:id" element={<PortfolioDetail />} />
-            <Route path="/portfolio/create" element={<PrivateRoute><PortfolioDetail /></PrivateRoute>} />
-            <Route path="/portfolio/edit/:id" element={<PrivateRoute><PortfolioDetail /></PrivateRoute>} />
-            <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
+    <AuthProvider>
+      <AdminProvider>
+        <Router>
+          <div className="min-h-screen bg-gray-100">
+            <Navbar />
+            <div className="container mx-auto px-4 py-8">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                {/* Normal kullanıcı giriş/kayıt kaldırıldı */}
+                <Route path="/blogs" element={<Blogs />} />
+                <Route path="/blog/:id" element={<BlogDetail />} />
+                <Route path="/create-blog" element={<PrivateRoute><CreateBlog /></PrivateRoute>} />
+                <Route path="/portfolio" element={<Portfolio />} />
+                <Route path="/portfolio/:id" element={<PortfolioDetail />} />
+                <Route path="/portfolio/create" element={<PrivateRoute><PortfolioDetail /></PrivateRoute>} />
+                <Route path="/portfolio/edit/:id" element={<PrivateRoute><PortfolioDetail /></PrivateRoute>} />
+                <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
 
-            <Route path="/admin/login" element={<AdminLogin />} />
-            <Route path="/admin" element={<AdminRoute><AdminDashboard /></AdminRoute>} />
-            <Route path="/admin/users" element={<AdminRoute><UserManagement /></AdminRoute>} />
-            <Route path="/admin/blogs" element={<AdminRoute><BlogManagement /></AdminRoute>} />
-            <Route path="/admin/comments" element={<AdminRoute><CommentManagement /></AdminRoute>} />
-            <Route path="/admin/portfolio" element={<AdminRoute><PortfolioManagement /></AdminRoute>} />
-            <Route path="/admin/about" element={<AdminRoute><AboutManagement /></AdminRoute>} />
-          </Routes>
-          <ToastContainer position="top-right" autoClose={3000} />
-        </div>
-      </div>
-    </AdminProvider>
+                {/* Admin route'ları */}
+                <Route path="/admin/login" element={<AdminLogin />} />
+                <Route path="/admin" element={<AdminRoute><AdminDashboard /></AdminRoute>} />
+                <Route path="/admin/users" element={<AdminRoute><UserManagement /></AdminRoute>} />
+                <Route path="/admin/blogs" element={<AdminRoute><BlogManagement /></AdminRoute>} />
+                <Route path="/admin/comments" element={<AdminRoute><CommentManagement /></AdminRoute>} />
+                <Route path="/admin/portfolio" element={<AdminRoute><PortfolioManagement /></AdminRoute>} />
+                <Route path="/admin/about" element={<AdminRoute><AboutManagement /></AdminRoute>} />
+              </Routes>
+              <ToastContainer position="top-right" autoClose={3000} />
+            </div>
+          </div>
+        </Router>
+      </AdminProvider>
+    </AuthProvider>
   );
 }
 
