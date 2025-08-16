76a49a64cce15768302569a51130092403edc4e9
omer <kasapoglu2100@gmail.com>
2025-08-16 20:49:56 +0300

    Add backend URL env variable7

--- sila-frontend/src/index.js
+++ sila-frontend/src/index.js
@@ -5,17 +5,17 @@ import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 import reportWebVitals from './reportWebVitals';
 import 'antd/dist/reset.css';
+import { HashRouter as Router } from 'react-router-dom'; // BrowserRouter yerine HashRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </AuthProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
